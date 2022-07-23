19c918f622b96e71700fc137030b305bb8438740
Andrew Lin <landrew051127@gmail.com>
2022-07-23 19:58:28 +0800

    playing around with firmware

---
+++ firmware/KFB/Forest65.c
@@ -0,0 +1,1 @@
+#include "Forest65"
--- firmware/KFB/config.h
+++ firmware/KFB/config.h
@@ -7,9 +7,9 @@
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x6060
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    qmkbuilder
-#define PRODUCT         keyboard
-#define DESCRIPTION     Keyboard
+#define MANUFACTURER    Timal
+#define PRODUCT         Forest65
+#define DESCRIPTION     top right blocker 65+10
 
 /* key matrix size */
 #define MATRIX_ROWS 5
