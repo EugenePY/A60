003cf715ed2b30d04ba106041844891f8f66d572
Andrew Lin <landrew051127@gmail.com>
2023-04-22 00:39:35 +0800

    update vendor id
    0x1127

--- firmware/forest65/config.h
+++ firmware/forest65/config.h
@@ -4,7 +4,7 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
+#define VENDOR_ID       0x1127
 #define PRODUCT_ID      0x6060
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Timal
@@ -17,7 +17,6 @@
 /* key matrix pins */
 #define MATRIX_ROW_PINS { F0, F1, F4, F5, F6 }
 #define MATRIX_COL_PINS { F7, C7, C6, D7, D6, D5, D4, D3, D2, D1, D0, B7, B6, B5, B4, B3, B2, B1 }
-#define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION ROW2COL
@@ -44,7 +43,6 @@
 
 
 #ifdef RGB_DI_PIN
-#define RGBLIGHT_ANIMATIONS
 #define RGBLED_NUM 0
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
